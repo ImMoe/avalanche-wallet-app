2a77352ce2b7719f798c97133231bfbfbd8249dc
Mohamed Ali <mohamed.557@icloud.com>
2024-10-16 22:03:48 +0200

    Modify styling

--- app/_components/Features.tsx
+++ app/_components/Features.tsx
@@ -2,7 +2,7 @@ import { ArrowRightLeft, Lock, Zap } from 'lucide-react';
 
 const Features = () => {
   return (
-    <section className='w-full py-12 md:py-24 lg:py-32 bg-gray-900'>
+    <section className='w-full py-12 md:py-24 lg:py-32 bg-gray-100 text-gray-900'>
       <div className='px-4 md:px-6'>
         <h2 className='text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-12'>
           Features
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <div className='h-96 relative w-full overflow-hidden flex flex-col items-center justify-center rounded-lg'>
+    <div className='h-[500px] relative w-full overflow-hidden flex flex-col items-center justify-center rounded-lg'>
       <div className='absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none' />
       <Boxes />
-      <section className='w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gray-900'>
+      <section className='w-full justify-self-center py-12 md:py-24 lg:py-32 xl:py-48 bg-gray-900'>
         <div className='px-4 md:px-6'>
           <div className='flex flex-col items-center space-y-4 text-center'>
             <div className='space-y-4 z-30'>
-              <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-white'>
+              <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-gray-200'>
                 Decentralized Avalanche Exchange
               </h1>
               <p className='text-gray-200 md:text-xl'>
